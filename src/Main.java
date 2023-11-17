a6223497b9b20b933d036e19cd06aedf9cf56c4b
Osmanov Yakub <osmanov3010@gmail.com>
2023-11-17 11:26:01 +0100

    add x and y

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        int y = 0;
         Orange orange = new Orange();
         System.out.println(orange);
+        int x = 10;
+        System.out.println(x);
     }
 }
